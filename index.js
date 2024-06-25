0659c56f99cd05cdf57edd611c3f01ea659baf8f
Mrtn Schndlr <mrtnschndlr@gmail.com>
2024-06-25 08:40:07 +0200

    Update index.js
    refactor grid template

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ module.exports = plugin.withOptions(
                 ':root': {
                     '--narrow-max-w': '1200px',
                     '--narrow-px': '1rem',
-                    '--narrow-size': 'calc(var(--narrow-max-w) / 2)',
+                    '--narrow-size': 'min((var(--narrow-max-w) / 2), 50% - var(--narrow-px) * 2)',
                     '--content-max-w': '1500px',
                     '--content-px': '1rem',
-                    '--content-size': 'calc((var(--content-max-w) - var(--narrow-max-w)) / 2)',
+                    '--content-size': 'minmax(var(--content-px), calc((var(--content-max-w) - var(--narrow-max-w)) / 2))',
                     '--breakout-max-w': '1700px',
-                    '--breakout-px': '0rem',
-                    '--breakout-size': 'calc((var(--breakout-max-w) - var(--content-max-w)) / 2)',
+                    '--breakout-px': '1rem',
+                    '--breakout-size': 'minmax(var(--breakout-px), calc((var(--breakout-max-w) - var(--content-max-w)) / 2))',
+                    '--full-size': 'minmax(0, 1fr)',
                 }
             });
 
             addComponents({
                 [`.${className}`]: {
                     'display': 'grid',
-                    'grid-template-columns': `[full-start] minmax(var(--breakout-px), 1fr)
-                [breakout-start] minmax(var(--content-px), var(--breakout-size))
-                [content-start] minmax(var(--narrow-px), var(--content-size))
-                [narrow-start] min(50% - var(--narrow-px) - var(--content-px) - var(--breakout-px), var(--narrow-size))
-                [content-center] min(50% - var(--narrow-px) - var(--content-px) - var(--breakout-px), var(--narrow-size))
-                [narrow-end] minmax(var(--narrow-px), var(--content-size))
-                [content-end] minmax(var(--content-px), var(--breakout-size))
-                [breakout-end] minmax(var(--breakout-px), 1fr)
-                [full-end]`,
+                    'grid-template-columns': `[full-start] var(--full-size)
+                [breakout-start] var(--breakout-size)
+                [content-start] var(--content-size)
+                [narrow-start] var(--narrow-size)
+                [full-center breakout-center narrow-center content-center] var(--narrow-size) [narrow-end]
+                var(--content-size) [content-end]
+                var(--breakout-size) [breakout-end]
+                var(--full-size) [full-end]`,
                     '> *': {
                         'grid-column': 'content'
                     }
